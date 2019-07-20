f607b508e5accfbe6a48706071f8859bd934ef9b
Stefano D Minervini <minervini@mac.com>
2019-07-20 14:56:45 -0500

    ctreated Tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -13,6 +13,11 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("Example")]
+        [InlineData(33.524131, -86.724876, Taco Bell Birmingham / ... (Free trial * Add to Cart for a full POI info))]
+        [InlineData(30.445296, -87.240548, Taco Bell Pensacola / ... (Free trial * Add to Cart for a full POI info))]
+        [InlineData(33.889469, -84.057706, Taco Bell Lawrenceville / ... (Free trial * Add to Cart for a full POI info))]
+        [InlineData(32.364153, -86.269979, Taco Bell Montgomery / ... (Free trial * Add to Cart for a full POI info))]
+
         public void ShouldParse(string str)
         {
             // TODO: Complete Should Parse
@@ -21,9 +26,26 @@ namespace LoggingKata.Test
         [Theory]
         [InlineData(null)]
         [InlineData("")]
+
         public void ShouldFailParse(string str)
         {
-            // TODO: Complete Should Fail Parse
+            // Arrange
+            ITrackable Parse = new ITrackable Parse();
+
+            // Act
+            string actual = Parse.ITrackable ParseGet(str);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
+
+// Arrange
+Calculator Calc = new Calculator();
+
+// Act
+int actual = Calc.GetFactorial(number);
+
+// Assert
+Assert.Equal(expected, actual);
